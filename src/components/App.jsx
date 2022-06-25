a87e84c914677af0483de05235b770a003575f75
kesel <a.kiselyoff@gmail.com>
2022-06-25 22:50:10 +0300

    add Route index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { About } from "../pages/About";
 import { Home } from "../pages/Home";
+import { ProductDetails } from "../pages/ProductDetails";
 import { Products } from "../pages/Products";
-import { NotFound } from "../pages/NotFound";
-import { ProductDetails } from "pages/ProductDetails";
 import { Mission } from "./Mission";
-import { Reviews } from "./Reviews";
 import { Team } from "./Team";
-import { Container, Header, Logo, Link } from "./App.styled";
+import { Reviews } from "./Reviews";
+import { SharedLayout } from "./SharedLayout";
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{" "}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
-           <Route path="mission" element={<Mission />} />
-           <Route path="team" element={<Team />} />
-           <Route path="reviews" element={<Reviews />} />
-
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
-        
-      </Routes>
-    </Container>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
